36e1e3686d0915a39a36b52e376c578b6aa1eb65
James Leslie <jamesleslie@posteo.net>
2024-09-27 21:28:59 +0100

    Update to use linear types properly and to use the Or protocol

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,19 +1,31 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTSyntax #-}
 {-# LANGUAGE LinearTypes #-}
+{-# LANGUAGE QualifiedDo #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module MyLib where
 
 import Control.Concurrent
+import Control.Functor.Linear qualified as Linear
 import Data.Kind (Type)
 import Prelude.Linear (Ur (..))
+import Prelude.Linear qualified as L
+import System.IO.Linear qualified as System
 
 -- * Protocol Combinators
 
-data (:>:) :: Type -> Type -> Type
+data (:>:) msg next
 
-data (:<:) :: Type -> Type -> Type
+data (:<:) msg next
+
+data (:+:) p1 p2
+
+-- data Choose ps next
+
+data (:*:) p1 p2
+
+-- data Offer ps next
 
 data EndSesh :: Type
 
@@ -22,56 +34,78 @@ data EndSesh :: Type
 type family Dual (p :: Type) :: Type where
   Dual (msg :>: next) = msg :<: Dual next
   Dual (msg :<: next) = msg :>: Dual next
+  Dual (p1 :+: p2) = Dual p1 :*: Dual p2
+  Dual (p1 :*: p2) = Dual p1 :+: Dual p2
   Dual EndSesh = EndSesh
 
 -- * Session (Phantom) types
 
 newtype Session channel protocol = Session (Ur channel)
 
-class Send channel msg where
-  send :: Session channel (msg :>: next) -> msg -> IO (Session channel next)
+class SendClass channel msg where
+  send :: Session channel (msg :>: next) %1 -> msg -> System.IO (Session channel next)
 
-class Recv channel msg where
-  recv :: Session channel (msg :<: next) %1 -> IO (msg, Session channel next)
+class RecvClass channel msg where
+  recv :: Session channel (msg :<: next) %1 -> System.IO (Ur msg, Session channel next)
+
+class OrClass channel p1 p2 where
+  or ::
+    Session channel (p1 :+: p2) %1 ->
+    Either (Session channel p1 %1 -> a) (Session channel p2 %1 -> a) ->
+    a
+
+class And channel p1 p2 where
+  and :: Session channel (p1 :*: p2) %1 -> System.IO (Session channel p1, Session channel p2)
 
 class End channel where
-  end :: Session channel EndSesh %1 -> IO ()
+  end :: Session channel EndSesh %1 -> System.IO ()
 
 -- | Channel implementation
-instance Send (Chan a) a where
-  send (Session (Ur chan)) msg = writeChan chan msg >> pure (Session (Ur chan))
+instance SendClass (Chan a) a where
+  send (Session (Ur chan)) msg =
+    System.fromSystemIOU (writeChan chan msg) Linear.>>= \(Ur _) -> Linear.pure (Session (Ur chan))
 
-instance Recv (Chan a) a where
-  recv (Session (Ur chan)) = readChan chan >>= \msg -> pure (msg, Session (Ur chan))
+instance RecvClass (Chan a) a where
+  recv (Session (Ur chan)) = System.fromSystemIOU (readChan chan) Linear.>>= \msg -> Linear.pure (msg, Session (Ur chan))
+
+instance OrClass (Chan a) p1 p2 where
+  or (Session (Ur channel)) (Left f) = f (Session (Ur channel))
+  or (Session (Ur channel)) (Right g) = g (Session (Ur channel))
+
+instance And (Chan a) p1 p2 where
+  and (Session (Ur chan)) = Linear.pure (Session (Ur chan), Session (Ur chan))
 
 instance End (Chan a) where
-  end (Session (Ur _)) = pure ()
+  end (Session (Ur _)) = Linear.pure ()
 
-type Protocol = String :>: (String :<: EndSesh)
+type Protocol = String :>: (String :<: (EndSesh :+: (String :<: EndSesh)))
 
-makeSession :: forall protocol msg. IO (Session (Chan msg) protocol, Session (Chan msg) (Dual protocol))
-makeSession = do
-  chan <- newChan @msg
-  chan' <- newChan @msg
-  return (Session (Ur chan), Session (Ur chan'))
+-- makeSession :: forall protocol msg. IO (Session (Chan msg) protocol, Session (Chan msg) (Dual protocol))
+-- makeSession = do
+--   chan <- newChan @msg
+--   chan' <- newChan @msg
+--   return (Session (Ur chan), Session (Ur chan'))
 
-impl :: Session (Chan String) Protocol -> IO ()
-impl chan = do
-  chan' <- send chan "a"
-  (msg, chan'') <- recv chan'
-  print msg
-  end chan''
+impl :: Session (Chan String) Protocol %1 -> System.IO ()
+impl chan = Linear.do
+  chan' <- send chan "hi"
+  (Ur msg, chan'') <- recv chan'
+  System.fromSystemIO (print msg)
+  case length msg of
+    0 -> MyLib.or chan'' L.$ Left end
+    _ -> MyLib.or chan'' L.$
+      Right $
+        \chan''' -> Linear.do
+          (Ur msg', chan'''') <- recv chan'''
+          System.fromSystemIO (print msg')
+          end chan''''
 
-revImpl :: Session (Chan String) (Dual Protocol) -> IO ()
-revImpl chan = do
-  (msg, chan') <- recv chan
-  print msg
-  chan'' <- send chan' "b"
-  end chan''
-
-foo :: IO ()
-foo = do
-  (sesh1, sesh2) <- makeSession @Protocol
-  t1 <- forkIO $ impl sesh1
-  t2 <- forkIO $ revImpl sesh2
-  threadDelay 1000000000
+revImpl :: Session (Chan String) (Dual Protocol) %1 -> System.IO ()
+revImpl chan = Linear.do
+  (Ur msg, chan') <- recv chan
+  System.fromSystemIO (print msg)
+  chan'' <- send chan' "Received"
+  (chan1, chan2) <- MyLib.and chan''
+  end chan1
+  chan3 <- send chan2 "woo"
+  end chan3
